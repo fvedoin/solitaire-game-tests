6148db88cf4a55d84be0f0c658859ec8c67b3575
Fernand <fernando.vgarcia@hotmail.com>
2021-01-18 00:57:38 -0300

    Tests on Solitaire class.

--- src/test/java/solitaire/SolitaireTests.java
+++ src/test/java/solitaire/SolitaireTests.java
@@ -1,4 +1,16 @@
 package solitaire;
 
+import com.fbergeron.solitaire.Solitaire;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
 public class SolitaireTests {
+    @Test
+    public void solitaireIsVisible_Success() {
+        Solitaire solitaire = new Solitaire(true);
+        solitaire.setVisible(true);
+        Boolean result = solitaire.isVisible();
+        assertTrue(result);
+    }
 }
